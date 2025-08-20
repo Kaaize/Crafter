077c4f9312892cc62f06186a82e72e1605d3e5d4
Kaaize <71920355+Kaaize@users.noreply.github.com>
2025-08-19 22:08:21 -0300

    Case

--- convert_name.py
+++ convert_name.py
@@ -50,6 +50,4 @@ def converter_nomes_arquivos(diretorio):
 # Exemplo de uso:
 # Altere 'meu_diretorio_com_arquivos' para o caminho do seu diretório.
 # Para o diretório atual, você pode usar '.'
-converter_nomes_arquivos(r'C:\Users\danie\Documents\GitHub\Crafter\imgs\drakantos\characters')
-converter_nomes_arquivos(r'C:\Users\danie\Documents\GitHub\Crafter\imgs\drakantos\orbs')
-converter_nomes_arquivos(r'C:\Users\danie\Documents\GitHub\Crafter\imgs\drakantos\trophies')
+converter_nomes_arquivos(r'C:\Users\danie\Documents\GitHub\Crafter\imgs\drakantos\orbs\IRIS\preview')
